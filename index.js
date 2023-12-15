b3c101505af6aab3de0e9e0007e2c258529bec0b
dijanakalaposratic <dijanakalaposratic@gmail.com>
2023-12-16 00:55:05 +0100

    added increment function

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 let count = 0;
 let display = document.getElementById("display");
 let saves = document.getElementById("saves");
+
+function increment() {
+	count++;
+	display.textContent = count;
+}
