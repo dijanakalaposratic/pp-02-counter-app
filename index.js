7c80589f545597b2745a46ec59ce5552918f9cd8
dijanakalaposratic <dijanakalaposratic@gmail.com>
2023-12-16 02:14:53 +0100

    added save function

--- index.js
+++ index.js
@@ -16,3 +16,20 @@ function reset() {
 	count = 0;
 	display.textContent = count;
 }
+function save() {
+	let savesNum = ` ${count}, `;
+	saves.textContent += savesNum;
+
+	// Check if the saves element is full
+	if (saves.scrollHeight > saves.clientHeight) {
+		saves.textContent = "Memory full. Please use the AC to clear the memory.";
+		// Disable the save button or take any other necessary action
+		document.getElementById("btn-save").disabled = true;
+	} else {
+		// Reset the display and count for the next input
+		display.textContent = 0;
+		count = 0;
+		// Enable the save button
+		document.getElementById("btn-save").disabled = false;
+	}
+}
